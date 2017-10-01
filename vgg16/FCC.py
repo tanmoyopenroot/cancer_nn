85de641672f218e9186a50d81fa66d81dac1b8ac
= <=>
2017-10-01 22:00:00 +0530

    abstracted FCC to a class, cleaned load data code, moved to s separate file

---
+++ vgg16/FCC.py
@@ -0,0 +1,122 @@
+from keras.models import Sequential
+from keras.layers import Input, Dropout, Flatten, Dense
+from keras import optimizers
+
+import loadData
+
+class FCC:
+
+
+	def __init__(
+		self,
+		epochs, 
+		hidden_layers = 1,
+		neurons = [64, 1], 
+		lr = 1e-4, 
+		batch_size = 32, 
+		loss = 'binary_crossentropy',
+		optimizer = 'Adam',
+		train_data_path = "train_transfer_block3_pool_values.npy",
+		vald_data_path = "validation_transfer_block3_pool_values.npy",
+		mmap = 'r' # set to None if the dataset fits into memory
+	):
+
+		self.epochs = epochs
+		self.hidden_layers = hidden_layers
+		self.neurons = neurons
+		self.lr = lr
+		self.batch_size = batch_size
+		self.loss = loss
+		self.optimizer = optimizer
+		self.train_data_path = train_data_path
+		self.vald_data_path = vald_data_path
+		self.mmap = mmap
+
+
+	def loadFCC( self, input_shape ):
+
+		model = Sequential()
+
+		model.add( Flatten( input_shape = input_shape[1 : ] ) )
+
+		if self.hidden_layers == len(self.neurons) - 1:
+
+			for neuron in self.neurons[ : len(self.neurons) - 1 ]:
+				model.add(Dense(
+					neuron,
+					activation = 'relu'
+				))
+				model.add( Dropout(0.5) )
+
+			model.add( Dense( 
+				self.neurons[ len(self.neurons) - 1 ],
+				activation = "sigmoid" 
+			))
+
+			return model
+
+		else: 
+			print "layer and length of neurons list should be equal"
+
+			return False
+
+	
+	def trainFCC( self ):
+
+		train_data, vald_data = loadData.data( self.train_data_path, self.vald_data_path, self.mmap)
+		train_labels, vald_labels = loadData.labels()
+
+		model = self.loadFCC( train_data.shape )
+
+		if model: 
+
+			model.compile(
+				loss = self.loss,
+				optimizer = optimizers.Adam( self.lr ),
+				metrics = ['accuracy']
+			)
+
+			history = model.fit(
+				train_data, 
+				train_labels,
+				epochs = self.epochs,
+				batch_size = self.batch_size,
+				validation_data = ( vald_data, vald_labels ),
+				shuffle = True
+			)
+
+			loadData.plotTraining( history )
+		else:
+			print "error in instantiating model"
+
+	def testFCC( self ):
+
+		vald_data = loadData.valdData( self.vald_data_path, mmap )
+		_, vald_labels = loadData.labels()
+
+		model = self.loadFCC( vald_data.shape )
+
+		if model:
+		   	pred = model.predict(
+
+		        validation_data,
+		        batch_size = 16,
+		        verbose = 1
+		    )
+
+		else:
+			print "error in instantiating model"
+
+	
+
+s = FCC( 20, train_data_path = "vgg16-train-conv-block-5-fine-tune-feature-maps.npy",
+	vald_data_path = "vgg16-validation-conv-block-5-fine-tune-feature-maps.npy"
+)
+s.trainFCC()
+
+
+
+
+
+
+	
--- vgg16/vgg16ModelFineTuning.py
+++ vgg16/vgg16ModelFineTuning.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import tensorflow as tf
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
 
 from keras import backend as K
 from keras.layers import Input
@@ -15,101 +17,7 @@ from keras import applications
 from keras import regularizers
 from keras.models import Sequential
 from keras.layers import Input, Dropout, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator 
-img_width, img_height = 224, 224
 
-top_model_weights_path = "isic-vgg16-transfer-learning-07-l2-300e.h5"
-
-train_data_dir = '../data/train'
-validation_data_dir = '../data/validation'
-
-train_aug_data_dir = '../data/aug/train'
-validation_aug_data_dir = "../data/aug/validation"
-
-nb_train_samples = 9216
-nb_validation_samples = 2304
-
-epochs = 10
-
-batch_size = 16
-
-def getDataGenObject ( directory, class_mode ):
-
-    datagen = ImageDataGenerator(
-        rescale = 1./255,
-        # rotation_range = 40,
-        # width_shift_range = 0.1,
-        # height_shift_range = 0.1,
-        # shear_range = 0.1,
-        # zoom_range = 0.1,
-        # horizontal_flip = True,
-        # fill_mode = "nearest"
-    )
-
-    datagen_generator = datagen.flow_from_directory(
-        directory,
-        target_size = (img_height, img_width),
-        batch_size = batch_size,
-        class_mode = class_mode,
-        shuffle = False
-    )
-
-    return datagen_generator
-
-def getTrainDataGenObject( path = train_aug_data_dir, class_mode = None ):
-
-    return getDataGenObject( path, class_mode )
-
-def getValidationDataGenObject( path = validation_aug_data_dir, class_mode = None ):
-
-    return getDataGenObject( path, class_mode )
-
-
-
-
-def saveIntermediateTransferValues( layer_name = "block4_pool" ):
-
-    model = applications.VGG16( include_top = False, weights = "imagenet")
-
-    for layer in model.layers:
-        print layer.name
-
-    intermediate_model = Model( 
-        inputs = model.input,
-        outputs = model.get_layer(layer_name).output
-    )
-
-    train_transfer_values = intermediate_model.predict_generator(
-
-        getTrainDataGenObject(),
-        nb_train_samples // batch_size,
-        verbose = 1
-
-    )
-
-    print ( "Train transfer Values shape {0}".format(train_transfer_values.shape) )
-    np.save( open("train_transfer_intermediate_values.npy", "w"), train_transfer_values )
-
-    validation_transfer_values = intermediate_model.predict_generator(
-
-        getValidationDataGenObject(),
-        nb_validation_samples // batch_size,
-        verbose = 1
-    )
-
-    print ( "Validation transfer Values shape {0}".format(validation_transfer_values.shape) )
-    np.save( open("validation_transfer_intermediate_values.npy", "w"), validation_transfer_values )
-
-def toTensor( np_array ):
-    
-    return tf.convert_to_tensor( np_array, np.float32 )
-
-def toKerasTensor( input_tensor ):
-
-    if K.is_keras_tensor( input_tensor ):
-        return input_tensor
-    else:
-        return Input( tensor = input_tensor, shape = input_tensor.shape)
 
 def plotTraining(history):
     # list all data in history
@@ -144,32 +52,30 @@ def VGG16ConvBlockFive( pretrained_weights ):
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
-    WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
-
     model = Model( input_img, x )
 
     if pretrained_weights :
-        print "pretrained conv_block_5 weights loading"
-        weights_path = get_file(
-            'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5',
-            WEIGHTS_PATH_NO_TOP,
-            cache_subdir='models'
-        )
-        model.load_weights( weights_path, by_name = True )
+        print "finetuned conv_block_5 weights loading"
+
+        model.load_weights( 'fine-tune-vgg16.h5', by_name = True )
 
     return model
 
 def FCC( pretrained_weights ):
     
     model = Sequential()
-    model.add(Flatten(input_shape = (7, 7, 512) ))
-    model.add(Dense(100, activation = "relu"))
+    model.add(Flatten(input_shape = (28, 28, 256) ))
+
+    model.add(Dense(
+        64,
+        activation = "relu"
+    ))
     model.add(Dropout(0.3))
     model.add(Dense(1, activation = "sigmoid"))
 
     if pretrained_weights:
         print "pretrained FCC weights loading"
-        model.load_weights( 'isic-vgg16-transfer-learning.h5' )
+        model.load_weights( 'FCC-init-random-weights-on-finetuned-data.h5' )
 
     return model
 
@@ -179,7 +85,6 @@ def vgg16FromScratch():
     model = applications.VGG16(include_top = False, weights = None, input_shape = (224, 224, 3))
 
     return model
-
 
 def initModel( fine_tune = True ):
 
@@ -246,10 +151,64 @@ def initModel( fine_tune = True ):
     plotTraining(history)
     
 
+def testFCC():
+    
+    model = FCC( True )
+    validation_data = np.load(open("vgg16-validation-conv-block-5-fine-tune-feature-maps.npy"))
+    # validation_data = np.load(open("validation-transfer-values-vgg16.npy"))
+    validation_labels = np.array( [0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
+
+    pred = model.predict(
+
+        validation_data,
+        batch_size = 16,
+        verbose = 1
+    )
+
+    print "confusion matrix"
+
+    print confusion_matrix( np.round( pred ) , validation_labels, labels = [0, 1])
+
+    print roc_auc_score(validation_labels, pred)
+
+def trainFCC():
+
+    model = FCC( False )    
+
+    train_data = np.load( "train_transfer_block3_pool_values.npy", mmap_mode = 'r' )
+    # train_data = np.load( 'train-transfer-values-vgg16.npy' )
+    train_labels = np.array( [0] *  (9216 / 2) + [1] * (9216 / 2))
+
+    validation_data = np.load( "validation_transfer_block3_pool_values.npy", mmap_mode = 'r')
+    # validation_data = np.load(open("validation-transfer-values-vgg16.npy"))
+    validation_labels = np.array( [0] * (2304 / 2) + [1] * (2304/ 2))
+
+    model.compile(loss='binary_crossentropy',
+        #optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+        #optimizer = optimizers.RMSprop( lr = 1e-2 ),
+        optimizer = optimizers.Adam( lr = 1e-3 ),
+        metrics=['accuracy']
+    )
+
+
+    history = model.fit(train_data, train_labels, 
+        epochs = 30, 
+        batch_size = 64, 
+        validation_data = (validation_data, validation_labels),
+        shuffle = True
+       #callbacks = callbacks_list
+    )
+
+    #model.save_weights( 'FCC-init-random-weights-on-finetuned-data.h5')
+    #plotTraining(history)
 
 def main():
-    #saveIntermediateTransferValues();    
-    initModel( fine_tune = False )
+    # initModel( fine_tune = False )
+
+    trainFCC()
+    # testFCC()
+    
+
     
 if __name__ == '__main__':
     main()
